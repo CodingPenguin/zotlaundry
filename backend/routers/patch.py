a5b24fdbb827a51491e0a8f7afdc3af8342afd9c
CodingPenguin <nguyenhuydanny@gmail.com>
2022-02-26 02:19:43 -0800

    patchy worky nowy

--- backend/routers/patch.py
+++ backend/routers/patch.py
@@ -1,3 +1,17 @@
-def handle_patch_machines(machines, query, update):
-    updated_doc = machines.find_one_and_update(filter = query, update = update)
-    return updated_doc
+def handle_patch_machines(machines, data):
+    query = {
+        "community": data["community"],
+        "floor": data["floor"],
+        "number": data["number"]    
+    }
+    
+    updater = {
+        "$set": {
+            "state": data["state"],
+            "remainingTime": data["remainingTime"],
+            "timeStarted": data["timeStarted"]
+        }
+    }
+    
+    updated_machine = machines.find_one_and_update(filter=query, update=updater, return_document=True)
+    return updated_machine
