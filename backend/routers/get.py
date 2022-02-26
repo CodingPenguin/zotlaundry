b01bdc1f1720cb81c7d31317711714450ff9920f
CodingPenguin <nguyenhuydanny@gmail.com>
2022-02-26 00:57:43 -0800

    update get-req

--- backend/routers/get.py
+++ backend/routers/get.py
@@ -1,6 +1,2 @@
-import json
-
 def handle_get_machines(machines, query):
-    f = [machine for machine in (machines.find(query))]
-    print(f)
-    return f
+    return [machine for machine in (machines.find(query))]
