e178d1e01153045937d55271f4413d2e39b835b8
CodingPenguin <nguyenhuydanny@gmail.com>
2022-02-26 01:16:43 -0800

    get is correct now

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 import os, uuid, json
 from loguru import logger
-from flask import Flask, request, abort
+from flask import Flask, jsonify, request, abort
 from pymongo import MongoClient
 
 from dotenv import load_dotenv
 
 from routers.get import handle_get_machines
+
 load_dotenv()
 
 mongo = MongoClient(os.getenv('MONGO_URL'))
@@ -19,7 +20,8 @@ app = Flask(__name__)
 def handle_reqs():
     if request.method == 'GET':
         data = json.loads(request.data)
-        return handle_get_machines(machines, data)
+        found_machines = tuple(handle_get_machines(machines, data))
+        return jsonify(found_machines), 200
     elif request.method == 'POST':
         handle_post_machines(request)
     elif request.method == 'PATCH':
