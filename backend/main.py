663d14fef3cf17744e374b0eefb55808c9bf7f50
CodingPenguin <nguyenhuydanny@gmail.com>
2022-02-26 00:52:19 -0800

    stuff

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
-import os, uuid
+import os, uuid, json
 from loguru import logger
-from flask import Flask
+from flask import Flask, request, abort
 from pymongo import MongoClient
 
 from dotenv import load_dotenv
 
+from routers.get import handle_get_machines
 load_dotenv()
 
 mongo = MongoClient(os.getenv('MONGO_URL'))
@@ -14,18 +15,17 @@ machines = db.machines
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    unique_id = str(uuid.uuid4())
-    machines.insert_one(
-        {
-            '_id': unique_id,
-            'name': 'hello world',
-        }
-    )
-    machine = machines.find_one({'_id': unique_id})
-    logger.debug(machine)
-    return machine
+@app.route("/machines", methods=['GET', 'POST', 'PATCH'])
+def handle_reqs():
+    if request.method == 'GET':
+        data = json.loads(request.data)
+        return handle_get_machines(machines, data)
+    elif request.method == 'POST':
+        handle_post_machines(request)
+    elif request.method == 'PATCH':
+        handle_patch_machines(request)
+    else:
+        abort(405)
 
 if __name__ == "__main__":
     app.run()
