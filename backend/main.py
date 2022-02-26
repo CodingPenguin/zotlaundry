f199b1a6cef8e96461ed5008005ff7f10f707ab6
CodingPenguin <nguyenhuydanny@gmail.com>
2022-02-25 22:19:20 -0800

    yeah

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,31 @@
+import os, uuid
+from loguru import logger
 from flask import Flask
+from pymongo import MongoClient
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mongo = MongoClient(os.getenv('MONGO_URL'))
+
+db = mongo.main
+machines = db.machines
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    unique_id = str(uuid.uuid4())
+    machines.insert_one(
+        {
+            '_id': unique_id,
+            'name': 'hello world',
+        }
+    )
+    machine = machines.find_one({'_id': unique_id})
+    logger.debug(machine)
+    return machine
 
 if __name__ == "__main__":
     app.run()
